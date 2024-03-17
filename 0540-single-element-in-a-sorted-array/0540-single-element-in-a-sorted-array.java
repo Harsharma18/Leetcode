7bf3c06e4fceb959b122682aa7a296d79111fc82
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-03-17 17:24:57 +0530

    Time: 0 ms (100.00%), Space: 50.7 MB (8.82%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,60 +1,44 @@
-// class Solution {
-//     public int singleNonDuplicate(int[] nums) {
-//       int n= nums.length;
-       
-//         if(n==1){
-//             return nums[0];
-//         }
-//         if(nums[0]!=nums[1]){
-//             return nums[0];
-//         }
-//         if(nums[n-1]==nums[n-2]){
-//             return nums[n-1];
-//         }
-//          int start = 1;
-//         int end = nums.length-2;
-        
-        
-         
-//         while(start<end){
-//             int mid = (start+end)/2;
-             
-//             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-//                 return nums[mid];
-//             }
-//         //we are in left half and eleminate this because element found in right half 
-//         if((mid%2==1 && nums[mid]==nums[mid-1]) ||(mid%2==0 &&  nums[mid]==nums[mid+1])){
-//             start = mid+1;
-//         }else{
-//             end = mid-1;
-            
-//         }
-            
-//         }
-//         return -1;
-      
-        
-        
-        
-//     }
-// }
 class Solution {
-     public int singleNonDuplicate(int[] nums) {
-         int left = 0;
-         int right = nums.length;
-
-         while (left + 1 < right) {
-             int mid = left + (right - left) / 2;
-
-             if (nums[mid] != nums[mid - 1] && (mid + 1== nums.length || nums[mid] != nums[mid + 1])) {
-                 return nums[mid];
-             } else if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 != 0 && nums[mid] == nums[mid - 1])) {
-                 left = mid;
-             } else {
-                 right = mid;
-             }
-         }
-
-         return nums[left];
-     }
- }
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+       
+        if (n == 1) {
+            return nums[0];
+        }
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
+        }
+        
+        int start = 1;
+        int end = n - 2;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+            
+            if (nums[mid] == nums[mid - 1]) {
+                // If mid is odd, then the single element is to the right
+                if (mid % 2 == 1) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                // If mid is even, then the single element is to the right
+                if (mid % 2 == 0) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        
+        return -1; // This line should never be reached if input is valid
+    }
+}
