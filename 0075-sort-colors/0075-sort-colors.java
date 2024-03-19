8e2f0aa30fef1afa5ba0538319c1680bd1f6b7eb
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-03-19 17:44:33 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (36.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,34 +1,55 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-        for(int i=0;i<nums.length;i++){
-             if(nums[i]==0){
-            count0++;
-        }
-        if(nums[i]==1){
-            count1++;
-        }
-        if(nums[i]==2){
-            count2++;
-        }
+//         int count0 = 0;
+//         int count1 = 0;
+//         int count2 = 0;
+//         for(int i=0;i<nums.length;i++){
+//              if(nums[i]==0){
+//             count0++;
+//         }
+//         if(nums[i]==1){
+//             count1++;
+//         }
+//         if(nums[i]==2){
+//             count2++;
+//         }
+            
+//         }
+       
+//         for(int i=0;i<count0;i++){
+//             nums[i]=0;
+//         }
+//          for(int i=count0;i<count1+count0;i++){
+//             nums[i]=1;
+//         }
+//          for(int i=count0+count1;i<nums.length;i++){
+//             nums[i] = 2;
+//         }
+        
+      int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                int temp1 = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp1;
+                high--;
+            }
+        
             
         }
-       
-        for(int i=0;i<count0;i++){
-            nums[i]=0;
-        }
-         for(int i=count0;i<count1+count0;i++){
-            nums[i]=1;
-        }
-         for(int i=count0+count1;i<nums.length;i++){
-            nums[i] = 2;
-        }
         
-      
+        
     }
-   
           
         
 }
