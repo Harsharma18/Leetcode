c243bd0f3736dfb37053a6d11a721bbed16e82b0
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-03-21 06:04:30 +0530

    Time: 1 ms (88.04%), Space: 44.6 MB (81.60%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,17 +1,44 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int n=nums.length;
-        int maxProd = Integer.MIN_VALUE;
-       
-     for (int i = 0; i < n; i++) {
-            int prod = 1; // Reset prod to 1 for each i
-            for (int j = i; j < n; j++) {
-                  prod =prod* nums[j];
-                maxProd = Math.max(prod, maxProd);
-            }
-        }
       
-      return maxProd;
         
+        //o(n2) time and o(1) space 
+//         int n=nums.length;
+//         int maxProd = Integer.MIN_VALUE;
+       
+//      for (int i = 0; i < n; i++) {
+//             int prod = 1; // Reset prod to 1 for each i
+//             for (int j = i; j < n; j++) {
+//                   prod =prod* nums[j];
+//                 maxProd = Math.max(prod, maxProd);
+//             }
+//         }
+      
+//       return maxProd;
+        
+//     }
+// }
+        
+        //o(n) time and o(1) space 
+        int preffix =1;
+    int suffix =1;
+    int n = nums.length;
+    int max = Integer.MIN_VALUE;
+    for(int i=0;i<n;i++){
+        if(preffix==0){
+             preffix =1;
+        }
+            
+       
+        if(suffix==0){
+         suffix=1;
+        }
+            preffix = preffix*nums[i];
+            suffix = suffix*nums[n-i-1];
+        
+       
+        max= Math.max(max,Math.max(preffix,suffix));
     }
+    return max;
+}
 }
