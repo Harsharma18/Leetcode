e3067f0ebec0e988ab58221b88178d2d7a2baa92
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-03-22 08:25:58 +0530

    Time: 4 ms (58.73%), Space: 85.4 MB (13.53%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,18 +1,47 @@
 class Solution {
-    public int[] rearrangeArray(int[] nums) {
+    public int[] rearrangeArray(int[] nums) {//brute force o(n+n/2)time and space o(n)
         int n = nums.length;
-        int ans[] = new int[n];
-        int posindex =0;
-        int negindex =1;
+        int pos[] = new int[n];
+        int neg[] = new int[n];
+       int  posindex = 0;
+        int negindex =0;
         for(int i=0;i<n;i++){
             if(nums[i]>0){
-                ans[posindex] = nums[i];
-                posindex = posindex+2;
+                pos[posindex] = nums[i];
+                posindex++;
             }else{
-                ans[negindex] = nums[i];
-                negindex = negindex+2;
+                neg[negindex] = nums[i];
+                negindex++;
             }
         }
-        return ans;
+        for(int i=0;i<n/2;i++){
+            nums[2*i] = pos[i];
+            nums[2*i+1] = neg[i];
+            
+        }
+        return nums;
+        
+        
+        
+        
+        
+        
+        //Time Complexity and space complexity:O(n)
+        // int n = nums.length;
+        // int ans[] = new int[n];
+        // int posindex =0;
+        // int negindex =1;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>0){
+        //         ans[posindex] = nums[i];
+        //         posindex = posindex+2;
+        //     }else{
+        //         ans[negindex] = nums[i];
+        //         negindex = negindex+2;
+        //     }
+        // }
+        // return ans;
+        
+        
     }
 }
