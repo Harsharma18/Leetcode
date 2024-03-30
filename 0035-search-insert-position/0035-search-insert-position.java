5f2860b1bf286ce71a01e9fae0f6812aad559ed9
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-03-30 08:27:41 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (76.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,5 +1,6 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        //o(logn) time and o(1) space complexity
           int n = nums.length; // size of the array
         int low = 0, high = n - 1;
         int ans = n;
