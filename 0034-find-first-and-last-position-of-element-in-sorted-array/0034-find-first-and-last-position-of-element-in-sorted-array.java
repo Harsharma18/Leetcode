317b868ebf393c1fb4baf03fc927aceb493a17fa
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-03-30 19:24:53 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (64.09%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -21,25 +21,68 @@ class Solution {
 //         return new int[]{-1, -1}; // Target element not found
 //     }
 // }
-           int n = nums.length;
-        int first = -1;
-        int last = -1;
         
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == target) {
-                first = i;
-                last = i;
-                break;
-            }
-        }
+        //or
+//            int n = nums.length;
+//         int first = -1;
+//         int last = -1;
         
-        if (first != -1) {
-            while (last < n && nums[last] == target) {
-                last++;
-            }
-            return new int[]{first, last-1}; // Return the range of occurrences
-        }
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] == target) {
+//                 first = i;
+//                 last = i;
+//                 break;
+//             }
+//         }
         
-        return new int[]{-1, -1}; // Target element not found
+//         if (first != -1) {
+//             while (last < n && nums[last] == target) {
+//                 last++;
+//             }
+//             return new int[]{first, last-1}; // Return the range of occurrences
+//         }
+        
+//         return new int[]{-1, -1}; // Target element not found
+//     }
+// }
+        
+        //o(logn)time and o(1)space complexity using lower and upper bound 
+        
+        int lb = lowerBound(nums,target);
+        int ub = upperBound(nums,target);
+        if(lb== nums.length || nums[lb]!=target){
+            return new int[]{-1,-1};
+}
+        return new int[]{lb,ub-1};
     }
+     public static int lowerBound(int nums[],int x){
+        int left=0;
+        int right = nums.length-1;
+            int ans = nums.length;
+            while(left<=right){
+                int mid = (left+right)/2;
+                if(nums[mid]>=x){
+                    ans = mid;
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            return ans;
+        }
+         public static int upperBound(int nums[],int x){
+        int left=0;
+        int right = nums.length-1;
+            int ans = nums.length;
+            while(left<=right){
+                int mid = (left+right)/2;
+                if(nums[mid]>x){
+                    ans = mid;
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+             return ans;
+        }
 }
