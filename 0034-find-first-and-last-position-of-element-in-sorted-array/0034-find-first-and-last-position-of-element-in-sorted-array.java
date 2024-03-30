b480fe06f1b6ff906ba41255b3f65cf5ffc202c4
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-03-30 16:40:48 +0530

    Time: 0 ms (100.00%), Space: 46.1 MB (8.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,20 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        // O(n) time and O(1) space
-        int n = nums.length;
+//         // O(n) time and O(1) space
+//         int n = nums.length;
+//         int first = -1;
+//         int last = -1;
+        
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] == target) {
+//                 if (first == -1) {
+//                     first = i; // Set first occurrence
+//                 }
+//                 last = i; // Update last occurrence
+//             }
+//         }
+        
+//         if (first != -1) {
+//             return new int[]{first, last}; // Return the range of occurrences
+//         }
+        
+//         return new int[]{-1, -1}; // Target element not found
+//     }
+// }
+           int n = nums.length;
         int first = -1;
         int last = -1;
         
         for (int i = 0; i < n; i++) {
             if (nums[i] == target) {
-                if (first == -1) {
-                    first = i; // Set first occurrence
-                }
-                last = i; // Update last occurrence
+                first = i;
+                last = i;
+                break;
             }
         }
         
         if (first != -1) {
+            while (last + 1 < n && nums[last + 1] == target) {
+                last++;
+            }
             return new int[]{first, last}; // Return the range of occurrences
         }
         
