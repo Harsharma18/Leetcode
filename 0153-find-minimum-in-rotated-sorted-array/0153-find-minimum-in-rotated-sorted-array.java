2300148e2ad087ce39ef476d61a9462ac2fcf179
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-04-02 14:55:04 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (12.12%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -12,20 +12,43 @@ class Solution {
 // }
         
         //o(logn) time and o(1) space
-        int left =0;
-        int right = nums.length-1;
-        int ans = Integer.MAX_VALUE;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[left]<=nums[mid]){
-                ans = Math.min(ans,nums[left]);
-                left = mid+1;
+//         int left =0;
+//         int right = nums.length-1;
+//         int ans = Integer.MAX_VALUE;
+//         while(left<=right){
+//             int mid=(left+right)/2;
+//             if(nums[left]<=nums[mid]){
+//                 ans = Math.min(ans,nums[left]);
+//                 left = mid+1;
                 
-            }else{
-               ans =  Math.min(ans,nums[mid]);
-                right = mid-1;
-            }
-        }
-        return ans;
+//             }else{
+//                ans =  Math.min(ans,nums[mid]);
+//                 right = mid-1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+//more optimize using binary search
+int left =0;
+int right = nums.length-1;
+int ans = Integer.MAX_VALUE;
+while(left<=right){
+    int mid = (left+right)/2;
+    if(nums[left]<=nums[right]){
+        ans = Math.min(ans,nums[left]);
+        break;
     }
+    if(nums[left]<=nums[mid]){
+        ans = Math.min(ans ,nums[left]);
+      left =   mid+1;
+    }else{
+        right=mid-1;
+        ans = Math.min(ans,nums[mid]);
+    }
+
+}
+return ans;
+}
 }
