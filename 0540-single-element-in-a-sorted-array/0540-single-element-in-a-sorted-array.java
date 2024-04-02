1e8109412e0b40de8eb01642f914cacb2fa271ed
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-04-02 18:21:23 +0530

    Time: 3 ms (6.56%), Space: 50.4 MB (37.25%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,47 +1,75 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-       
-        if (n == 1) {
+        //o(n) time and O(1)space 
+          int n = nums.length; // Size of the array.
+        if (n == 1)
             return nums[0];
-        }
-        if (nums[0] != nums[1]) {
-            return nums[0];
-        }
-        if (nums[n - 1] != nums[n - 2]) {
-            return nums[n - 1];
-        }
-        
-        int start = 1;
-        int end = n - 2;
-        
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
-                return nums[mid];
+
+        for (int i = 0; i < n; i++) {
+            // Check for first index:
+            if (i == 0) {
+                if (nums[i] != nums[i + 1])
+                    return nums[i];
             }
-            
-              if (mid % 2 == 1) {
-                // If mid is odd, then the single element is to the right
-                if (nums[mid] == nums[mid - 1]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+            // Check for last index:
+            else if (i == n - 1) {
+                if (nums[i] != nums[i - 1])
+                    return nums[i];
             } else {
-                // If mid is even, then the single element is to the right
-                 if (mid % 2 == 0) {
-                // If mid is odd, then the single element is to the right
-                if (nums[mid] == nums[mid +1]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            }
+                if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
+                    return nums[i];
             }
         }
-        
-        return -1; // This line should never be reached if input is valid
+
+        // Dummy return statement:
+        return -1;
     }
+        
 }
+
+        //o(logn)time and o(1)space 
+//         int n = nums.length;
+       
+//         if (n == 1) {
+//             return nums[0];
+//         }
+//         if (nums[0] != nums[1]) {
+//             return nums[0];
+//         }
+//         if (nums[n - 1] != nums[n - 2]) {
+//             return nums[n - 1];
+//         }
+        
+//         int start = 1;
+//         int end = n - 2;
+        
+//         while (start <= end) {
+//             int mid = (start + end) / 2;
+            
+//             if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+//                 return nums[mid];
+//             }
+            
+//               if (mid % 2 == 1) {
+//                 // If mid is odd, then the single element is to the right
+//                 if (nums[mid] == nums[mid - 1]) {
+//                     start = mid + 1;
+//                 } else {
+//                     end = mid - 1;
+//                 }
+//             } else {
+//                 // If mid is even, then the single element is to the right
+//                  if (mid % 2 == 0) {
+//                 // If mid is odd, then the single element is to the right
+//                 if (nums[mid] == nums[mid +1]) {
+//                     start = mid + 1;
+//                 } else {
+//                     end = mid - 1;
+//                 }
+//             }
+//             }
+//         }
+        
+//         return -1; // This line should never be reached if input is valid
+//     }
+// }
