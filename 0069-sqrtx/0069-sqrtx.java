9de683f7052669032ef46619752a8a00f67b9346
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-04-03 17:49:36 +0530

    Time: 23 ms (7.76%), Space: 40.8 MB (52.60%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,8 +1,5 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) {
-            return x;
-        }
         
         int ans = 0;
         for (int i = 0; i <= x; i++) {
