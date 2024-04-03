20f3691a5ffd45bb4a12724ceacc01e11223a3e7
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-04-04 01:35:26 +0530

    Time: 1 ms (86.48%), Space: 40.5 MB (78.66%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,15 +1,59 @@
 class Solution {
     public int mySqrt(int x) {
+        //o(n)time and o(1)space
+//         int ans = 0;
+//         for (int i = 0; i <= x; i++) {
+//             long val = (long) i * i; // Using long to avoid overflow
+//             if (val <= x) {
+//                 ans = i;
+//             } else {
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// }
         
+        //o(logn)time and o(1)space 
+          //o(logn) time and o(1) space 
+//         int left = 1;
+//         int right = x;
+//         int ans = 0;
+//         while (left <= right) {
+//             int mid = (left + right) / 2;
+//             long val = (long) mid * mid;
+//             if (val <= x) {
+//                 ans = mid;
+//                 left = mid + 1;
+//             } else {
+//                 right = mid - 1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+          if (x == 0 || x == 1) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x;
         int ans = 0;
-        for (int i = 0; i <= x; i++) {
-            long val = (long) i * i; // Using long to avoid overflow
-            if (val <= x) {
-                ans = i;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long val = (long) mid * mid;
+
+            if (val == x) {
+                return mid; // If square of mid equals x, mid is the square root
+            } else if (val < x) {
+                ans = mid;
+                left = mid + 1;
             } else {
-                break;
+                right = mid - 1;
             }
         }
+
         return ans;
     }
 }
