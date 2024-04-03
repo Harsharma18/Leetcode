6d69afaedbf85db5332913f4f2de7ce8f60cadbb
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-04-03 17:49:10 +0530

    Time: 22 ms (8.01%), Space: 41.1 MB (19.47%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        int sqrt = (int) Math.sqrt(x);
-        return sqrt;
+        if (x == 0 || x == 1) {
+            return x;
+        }
         
+        int ans = 0;
+        for (int i = 0; i <= x; i++) {
+            long val = (long) i * i; // Using long to avoid overflow
+            if (val <= x) {
+                ans = i;
+            } else {
+                break;
+            }
+        }
+        return ans;
     }
 }
