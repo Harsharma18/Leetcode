a8d88d133b18d1e7883697e763feed37fa74ab95
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-04-05 08:53:05 +0530

    Time: 27 ms (29.97%), Space: 44.9 MB (58.27%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,32 +1,32 @@
 class Solution {
-    public static int findMax(int piles[]){
-        int max = Integer.MIN_VALUE;
-        for(int i=0;i<piles.length;i++){
-            max = Math.max(max,piles[i]);
-        }
-        return max;
-    }
-    public static int calculateTotalHours(int[] piles, int hourly) {
-        int totalH = 0;
-        int n = piles.length;
-        //find total hours:
-        for (int i = 0; i < n; i++) {
-            totalH += Math.ceil((double)(piles[i]) / (double)(hourly));
-        }
-        return totalH;
-    }
     public int minEatingSpeed(int[] piles, int h) {
-          int left = 1;
-        int right = findMax(piles);
-        while(left<=right){
-            int mid = (left+right)/2;
-            int calculate = calculateTotalHours(piles, mid);
-            if(calculate<=h){
-                right=mid-1;
-            }else{
-                left = mid+1;
+          int low = 1, high = Integer.MIN_VALUE;
+        int n = piles.length;
+        
+        // Find the maximum number of bananas
+        for (int i = 0; i < n; i++) {
+            high = Math.max(high, piles[i]);
+        }
+
+        // Apply binary search
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int sum = 0;
+            
+            // Calculate total hours
+            for (int i = 0; i < n; i++) {
+                sum += Math.ceil((double) piles[i] / (double) mid);
+            }
+            
+            // Adjust the search boundaries
+            if (sum <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return left;
+        
+        return low;
+        
     }
 }
