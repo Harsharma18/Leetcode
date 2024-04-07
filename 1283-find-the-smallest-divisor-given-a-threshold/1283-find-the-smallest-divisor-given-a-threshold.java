b03040c337baa461bb98339ec2cfd8cb1433d276
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-04-07 05:55:50 +0530

    Time: 31 ms (33.82%), Space: 48.4 MB (9.35%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,5 +1,34 @@
 class Solution {
+     public static int sumByD(int[] arr, int div) {
+        int n = arr.length; //size of array
+        //Find the summation of division values:
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.ceil((double)(arr[i]) / (double)(div));
+        }
+        return sum;
+    }
     public int smallestDivisor(int[] nums, int threshold) {
+        //Time Complexity: O(n * max)
+         //Space Complexity: O(1)
+//          int max = Integer.MIN_VALUE;
+//         for (int i = 0; i < nums.length; i++) {
+//             max = Math.max(max, nums[i]);
+//         }
+        
+//         for (int th = 1; th <= max; th++) {
+//             int sum = 0;
+//             for (int j = 0; j < nums.length; j++) {
+//                 sum += Math.ceil((double) nums[j] / th);
+//             }
+//             if (sum <= threshold) {
+//                 return th;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+        //binary Search O(log(max(arr[]))*N),time and space is o(1)
      int left = 1;
      int right = Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
@@ -7,14 +36,21 @@ class Solution {
         }
         while(left<=right){
             int mid =  (left+right)/2;
-            int sum =0;
-            for(int i=0;i<nums.length;i++){
-                sum += Math.ceil((double)nums[i]/(double)mid);
-            }
-            if(sum<=threshold){
-                right = mid-1;
-            }else{
-            left = mid+1;
+            // int sum =0;
+            // for(int i=0;i<nums.length;i++){
+            //     sum += Math.ceil((double)nums[i]/(double)mid);
+            // }
+            // if(sum<=threshold){
+            //     right = mid-1;
+            // }else{
+            // left = mid+1;
+            // }
+            
+            //or
+             if (sumByD(nums, mid) <= threshold) {
+                right= mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return left;
