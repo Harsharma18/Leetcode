6af26128c9b2cda437c4226904d6b398a9d1532e
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-05-03 08:28:43 +0530

    Time: 2 ms (63.45%), Space: 45.8 MB (62.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,34 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] merged = new int[m + n];
-        int i = 0, j = 0, k = 0;
-
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                merged[k++] = nums1[i++];
-            } else {
-                merged[k++] = nums2[j++];
-            }
+        int i=0 ,j=0,k=0;
+        int m[] = new int[nums1.length+nums2.length];
+        while(i<nums1.length&&j<nums2.length){
+         if(nums1[i]<nums2[j]){
+             m[k] = nums1[i];
+             i++;
+             k++;
+             
+         }else{
+             m[k]= nums2[j];
+             j++;
+             k++;
+         }
         }
-
-        while (i < m) {
-            merged[k++] = nums1[i++];
+        
+        //rest of elements
+        while(i<nums1.length){
+            m[k] = nums1[i];
+            i++;
+            k++;
+            
         }
-
-        while (j < n) {
-            merged[k++] = nums2[j++];
+        while(j<nums2.length){
+            m[k]=nums2[j];
+            j++;
+            k++;
         }
-
-        if ((m + n) % 2 == 0) {
-            // Even number of elements, return the average of the middle two
-            int mid1 = merged[(m + n) / 2 - 1];
-            int mid2 = merged[(m + n) / 2];
-            return (double) (mid1 + mid2) / 2;
-        } else {
-            // Odd number of elements, return the middle element
-            return merged[(m + n) / 2];
+        //find median 
+        if(m.length%2==0){
+          int mid = m.length/2;
+            return (double)((m[mid]+m[mid-1])/2.0);
+        }else{
+           int mid = m.length/2;
+            return m[mid];
         }
+     
     }
 }
