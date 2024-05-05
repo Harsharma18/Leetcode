a2f5c6454286910a8d334a248895fdef20ff4211
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-05-05 08:00:33 +0530

    Time: 6 ms (48.68%), Space: 45.8 MB (49.13%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,27 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Check for edge cases
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
-            return false;
-        
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        
-        int row = 0;
-        int col = cols - 1;
-        
-        // Start from the top-right corner
-        // If the current element is greater than target, move left
-        // If the current element is less than target, move down
-        while (row < rows && col >= 0) {
-            if (matrix[row][col] == target)
-                return true;
-            else if (matrix[row][col] < target)
-                row++;
-            else
-                col--;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i=0;i<n;i++){
+            int left =0;
+            int right = m-1;
+            while(left<=right){
+                int mid = (left+right)/2;
+                if(matrix[i][mid]==target){
+                    return true;
+                }else if(matrix[i][mid]<target){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
         }
-        
         return false;
     }
 }
