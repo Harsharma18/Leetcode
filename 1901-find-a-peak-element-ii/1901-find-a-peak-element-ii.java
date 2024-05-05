887a75b71e6d587e3c12e4a264294158d663bfa2
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-05-05 15:35:15 +0530

    Time: 4 ms (7.72%), Space: 89.5 MB (62.90%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,22 +1,28 @@
-public class Solution {
+class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        
-        int[] peakIndices = new int[2];
-        int max = Integer.MIN_VALUE;
-        
-        // Search for the peak element
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] > max) {
-                    max = mat[i][j];
-                    peakIndices[0] = i;
-                    peakIndices[1] = j;
+        int n = mat.length;
+        int m = mat[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                boolean isPeak = true;
+                if(i>0 && mat[i][j]<mat[i-1][j]){
+                    isPeak = false;
+                }
+                else if(i<n-1 && mat[i][j]<mat[i+1][j]){
+                    isPeak = false;
+                }
+                else if(j>0 && mat[i][j]<mat[i][j-1]){
+                    isPeak = false;
+                }
+                else if(j<m-1 && mat[i][j]<mat[i][j+1]){
+                    isPeak = false;
+                }
+                if(isPeak==true){
+                    return new int[]{i,j};
                 }
             }
         }
-        
-        return peakIndices;
+        return new int[]{-1,-1};
+       
     }
-}
+}      
