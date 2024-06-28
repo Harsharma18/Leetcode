4eecef2a5cd9bbe395edb8d9a4ba0e77f34c7b9c
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-06-28 16:20:14 +0530

    Time: 0 ms (100.00%), Space: 45 MB (89.50%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,13 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       Arrays.sort(nums);
-        for(int i=1;i<nums.length;i=i+3){
-            if(nums[i]!=nums[i-1]){
-                return nums[i-1];
-            }
+        //iteration = o(n) and sorting o(nlogn ) overall tc is o(nlogn) and space o(1)
+//        Arrays.sort(nums);
+//         for(int i=1;i<nums.length;i=i+3){
+//             if(nums[i]!=nums[i-1]){
+//                 return nums[i-1];
+//             }
+//         }
+//         return nums[nums.length-1];
+        
+      //  o(n) time  complexty and o(1) space complexity 
+              int ones = 0;
+               int twos = 0;
+        for(int i=0;i<nums.length;i++){
+              ones = ((ones^nums[i]) & ~twos);
+              twos = ((twos^ nums[i]) & ~ones);
         }
-        return nums[nums.length-1];
-        
-        
+        return ones;
     }
 }
