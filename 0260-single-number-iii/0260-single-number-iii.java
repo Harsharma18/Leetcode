14eb9f98944d5ef05253ebff896dea4b8faf8f89
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-07-02 16:59:43 +0530

    Time: 1 ms (99.97%), Space: 45.6 MB (46.18%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,23 +1,30 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int xor = 0;
-        for(int i=0;i<nums.length;i++){
-             xor = xor^nums[i];
-
+        // Arrays.sort(nums); if i write o(nlogn tc come )
+        //o(n) time complexity and o(1)space complexity
+         int xor = 0;
+        for (int i = 0; i < nums.length; i++) {  // Find XOR of all elements
+            xor = xor ^ nums[i];
         }
-        int lowestBit = xor & -xor;
+        // Calculate the two's complement
+        int twosComplement = ~xor + 1;
+        
+        // Find the rightmost set bit
+        int lowestBit = xor & twosComplement;
+        
+        //or
+     //   int lowestBit = xor & -xor;  // Find the rightmost set bit
+        
         int result[] = new int[2];
-        for(int i=0;i<nums.length;i++){
-            if((lowestBit & nums[i] )==0){
-                result[0] = result[0]^nums[i];
-                
-            }else{
-                result[1] = result[1]^nums[i];
-                
+        for (int i = 0; i < nums.length; i++) {  // Separate numbers into two groups                                                     and XOR separately
+            if ((lowestBit & nums[i]) == 0) {
+                result[0] = result[0] ^ nums[i];
+            } else {
+                result[1] = result[1] ^ nums[i];
             }
         }
-        return result;
+        
+        return result;  // Return the result 
         
         
     }
