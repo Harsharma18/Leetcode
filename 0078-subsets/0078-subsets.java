a938f8f91738c89807060ce7037bb83af3af576f
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-07-02 20:13:44 +0530

    Time: 1 ms (52.12%), Space: 42.4 MB (84.49%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,27 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>>ans  = new ArrayList<>();
-        int subsets = 1<<nums.length;
-        for(int i=0;i<subsets;i++){
-            List<Integer>list = new ArrayList<>();
-            for(int j=0;j<nums.length;j++){
-               if((i &(1<<j))!=0){
-                   list.add(nums[j]);
-                   
-               }
-               
+     
+        // Initialize a list to store all subsets
+           List<List<Integer>> ans = new ArrayList<>(); 
+        // Calculate total number of subsets (2^nums.length)
+        int subsets = 1 << nums.length;  
+        // Iterate through all possible subsets (from 0 to subsets - 1)
+        for (int i = 0; i < subsets; i++) {  
+             // Initialize a list to store current subset
+            List<Integer> list = new ArrayList<>(); 
+            // Iterate through each element in nums array
+            for (int j = 0; j < nums.length; j++) {  
+                // Check if jth bit of i is set
+                if ((i & (1 << j)) != 0) {  
+                     // Add nums[j] to the current subset if the jth bit is set
+                    list.add(nums[j]); 
+                }
             }
-             ans.add(list);
+            // Add current subset to the final list of subsets
+            
+            ans.add(list);  
         }
-        return ans;
-        
+        // Return the list of all subsets
+        return ans;  
     }
 }
