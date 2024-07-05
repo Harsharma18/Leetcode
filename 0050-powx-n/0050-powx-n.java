727a0627e38062235a56b5d68e65a4419c5f4bc6
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-07-05 23:06:54 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (44.40%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -4,16 +4,18 @@ class Solution {
             return 1.0;
         }
         
-        double temp = myPow(x, n / 2);
+        double ans = 1;
+        long absN = Math.abs((long)n); // Use long to handle the edge case for Integer.MIN_VALUE
         
-        if (n % 2 == 0) {
-            return temp * temp;
-        } else {
-            if (n > 0) {
-                return x * temp * temp;
-            } else {
-                return (temp * temp) / x;
+        while (absN > 0) {
+            if (absN % 2 == 1) {
+                ans *= x;
+                absN = absN-1;
             }
+            x *= x;
+            absN /= 2;
         }
+        
+        return n < 0 ? 1.0 / ans : ans;
     }
 }
