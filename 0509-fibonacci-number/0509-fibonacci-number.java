d3a292d2c2d38fada1cf5c5c04e4d4d2780f7fdf
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-07-09 05:08:34 +0530

    Time: 8 ms (49.85%), Space: 40.2 MB (49.59%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,10 +3,10 @@ class Solution {
         if(n==0 || n==1){
             return n;
         }
-        int  fibN1 = fib(n-1);
-        int fibN2 = fib(n-2);
-      int   fn = fibN1+fibN2;
-        return fn;
+      //   int  fibN1 = fib(n-1);
+      //   int fibN2 = fib(n-2);
+      // int   fn = fibN1+fibN2;
+        return fib(n-1)+fib(n-2);
        
     
             
