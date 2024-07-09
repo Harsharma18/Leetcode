487b01525ad288a5176dbf80bab475cae01d55c8
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-07-09 09:36:31 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (72.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,28 +1,11 @@
 class Solution {
-    public double myPow(double x, int n) {
-        // Base case: if the exponent is 0, return 1.0
-        // Time complexity: O(1)
-        if (n == 0) {
-            return 1.0;
-        }
-
-        double ans = 1;  // Initialize answer
-        double N = Math.abs((double)n);  // Take the absolute value of n
-        // Space complexity: O(1), constant extra space used for ans and N
-
-        // Loop to calculate x^n
-        // Time complexity: O(log n), since we are dividing N by 2 in each iteration
-        while (N > 0) {
-            if (N % 2 == 1) {  // If N is odd, multiply ans by x
-                ans = ans * x;
-                N = N - 1;
-            }
-            x = x * x;  // Square the base
-            N = N / 2;  // Divide N by 2
-        }
-
-        // If n is negative, return the reciprocal of ans
-        // Time complexity: O(1)
-        return n < 0 ? 1.0 / ans : ans;
-    }
+  public double myPow(double x, long n) {
+    if (n == 0)
+      return 1;
+    if (n < 0)
+      return 1 / myPow(x, -n);
+    if (n % 2 == 1)
+      return x * myPow(x, n - 1);
+    return myPow(x * x, n / 2);
+  }
 }
