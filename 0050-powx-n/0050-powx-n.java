5daebd2ba7552f7f2ba67fc2b3203f2007a0eb8a
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-07-09 10:20:06 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (28.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,11 +1,20 @@
 class Solution {
-  public double myPow(double x, long n) {
-    if (n == 0)
-      return 1.0;
-    if (n < 0)
-      return 1 / myPow(x, -n);
-    if (n % 2 == 1)
-      return x * myPow(x, n - 1);
-    return myPow(x * x, n / 2);
-  }
+    public double myPow(double x, int n) {
+        return myPow(x, (long) n);
+    }
+
+    private double myPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        if (n < 0) {
+            return 1.0 / myPow(x, -n);
+        }
+        double halfPower = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return halfPower * halfPower;
+        } else {
+            return halfPower * halfPower * x;
+        }
+    }
 }
