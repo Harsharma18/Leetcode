e7ea431e2d3842d869111829a4292a6ea1178722
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-07-21 19:29:32 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (71.39%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,4 +1,34 @@
+// // o(n*2^n) t.c and s.c
 // class Solution {
+//     // Method to return all subsets of the given array
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         int n = nums.length;
+//         int totalSubsets = 1 << n; // Total number of subsets is 2^n
+
+//         // Iterate over all possible combinations
+//         for (int i = 0; i < totalSubsets; i++) {
+//             List<Integer> subset = new ArrayList<>();
+//             for (int j = 0; j < n; j++) {
+//                 // Check if the j-th bit in i is set
+//                 if ((i & (1 << j)) != 0) {
+//                     subset.add(nums[j]);
+//                 }
+//             }
+//             result.add(subset);
+//         }
+
+//         return result;
+//     }
+// }
+
+
+
+
+
+
+//or
+//class Solution {
 
 //     public static void Subsets(List<List<Integer>> result, int nums[]) {
 //         int n = nums.length;
@@ -24,25 +54,29 @@
 //         return result;
 //     }
 // }
-class Solution {
-    // Method to return all subsets of the given array
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-        int totalSubsets = 1 << n; // Total number of subsets is 2^n
+//using recurson 
 
-        // Iterate over all possible combinations
-        for (int i = 0; i < totalSubsets; i++) {
-            List<Integer> subset = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
-                // Check if the j-th bit in i is set
-                if ((i & (1 << j)) != 0) {
-                    subset.add(nums[j]);
-                }
-            }
-            result.add(subset);
+class Solution {
+    // Helper method to generate all subsets using recursion
+    private void helper(int[] nums, int index, List<Integer> curr, List<List<Integer>> res) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(curr)); // Add the current subset to the result
+            return;
         }
 
-        return result;
+        // Exclude the current element
+        helper(nums, index + 1, curr, res);
+
+        // Include the current element
+        curr.add(nums[index]);
+        helper(nums, index + 1, curr, res);
+        curr.remove(curr.size() - 1); // Backtrack to remove the last element before returning
+    }
+
+    // Method to return all subsets of the given array
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(nums, 0, new ArrayList<>(), res);
+        return res;
     }
 }
