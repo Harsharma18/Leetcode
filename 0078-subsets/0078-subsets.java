bf2e36c7bbde2d62d8d10d7b724676f647090724
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-07-21 15:23:52 +0530

    Time: 1 ms (51.77%), Space: 42.4 MB (85.04%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,6 +1,33 @@
+// class Solution {
+
+//     public static void Subsets(List<List<Integer>> result, int nums[]) {
+//         int n = nums.length;
+//         int totalSubsets = 1 << n; // Total number of subsets is 2^n
+
+//         // Iterate over all possible combinations
+//         for (int i = 0; i < totalSubsets; i++) {
+//             List<Integer> subset = new ArrayList<>();
+//             for (int j = 0; j < n; j++) {
+//                 // Check if the j-th bit in i is set
+//                 if ((i & (1 << j)) != 0) {
+//                     subset.add(nums[j]);
+//                 }
+//             }
+//             result.add(subset);
+//         }
+//     }
+
+//     // Method to return all subsets of the given array
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         Subsets(result, nums);
+//         return result;
+//     }
+// }
 class Solution {
-    // Helper method to generate all subsets using bit manipulation
-    public static void Subsets(List<List<Integer>> result, int nums[]) {
+    // Method to return all subsets of the given array
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
         int totalSubsets = 1 << n; // Total number of subsets is 2^n
 
@@ -15,12 +42,7 @@ class Solution {
             }
             result.add(subset);
         }
-    }
 
-    // Method to return all subsets of the given array
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Subsets(result, nums);
         return result;
     }
 }
