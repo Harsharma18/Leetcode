44303b051d24439bcf7da74c8e6b821e00476d38
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-08-12 09:58:14 +0530

    Time: 2 ms (84.22%), Space: 44.4 MB (65.02%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,19 +1,21 @@
+//0(2^n*k)time and o(n*k) space
 class Solution {
     public static void helper(int arr[],int target,int index,List<List<Integer>>res,List<Integer>currList){
         if(index==arr.length){
             if(target==0){
-                res.add(new ArrayList<>(currList));
+                res.add(new ArrayList<>(currList)); //not takeo(1) it take o(n)
             }
             return;
         }
-        //exclude 
-        helper(arr,target,index+1,res,currList);
-        //include
+       
+        //include picked up
        if(arr[index]<=target){
            currList.add(arr[index]);
            helper(arr,target-arr[index],index,res,currList);
-           currList.remove(currList.size()-1);
+           currList.remove(currList.size()-1); //backtrack
        }
+         //exclude  not pick
+        helper(arr,target,index+1,res,currList);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
        List<List<Integer>>res = new ArrayList<>();
