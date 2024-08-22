dbbea1eac1b06ff1565edc67ad91784922d29c8a
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-08-22 09:28:58 +0530

    Time: 3 ms (58.39%), Space: 45 MB (26.94%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,70 @@
+class Solution {
+    public static boolean isSafe(char board[][],int row ,int col){
+        //vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+}
+        //diagonal left up 
+         for(int i=row-1,j=col-1;j>=0&&i>=0;j--,i--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+}
+        //diagnol right up 
+         for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+}
+        return true;
+        
+    }
+      public static void ArrangeNQueen(char board[][],int row,List<List<String>>result){
+          if(row==board.length){
+             result.add(printBoard(board));
+              
+              return;
+          
+          }
+          //Column loop
+          for(int col=0;col<board.length;col++){
+              if(isSafe(board,row,col)){
+                   board[row][col] = 'Q';
+              ArrangeNQueen(board,row+1,result);
+              board[row][col] = '.';
+                  
+              }
+             
+}
+          
+       
+      }
+   public static List<String> printBoard(char[][] board) {
+    List<String> printboard = new ArrayList<>();
+    
+    for (int i = 0; i < board.length; i++) {
+        StringBuilder row = new StringBuilder();  // Create a StringBuilder for each row
+        for (int j = 0; j < board[i].length; j++) {
+            row.append(board[i][j]);  // Efficiently append each character
+        }
+        printboard.add(row.toString());  // Convert the row to a String and add to the list
+    }
+    
+    return printboard;
+}
+
+    public   List<List<String>> solveNQueens(int n) {
+        List<List<String>>result = new ArrayList<>();
+        char board[][] = new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] = '.';
+            }
+        }
+        ArrangeNQueen(board,0,result);
+        return result;
+        
+    }
+}
