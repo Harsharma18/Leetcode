15b1d8c8e8cc9592cebefe18abca9bfe6daf4835
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-08-26 09:57:27 +0530

    Time: 10 ms (25.34%), Space: 40.4 MB (63.30%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,12 +1,49 @@
 class Solution {
-    public static int winner(int n ,int k ){
-        if(n==1){
-            return 0;
-        }
-        return (winner(n-1,k) + k)%n;
+    public static int winner(int n ,int k,int person[],int personleft,int index){
+      if(personleft==1){
+          for(int i=0;i<n;i++){
+           if(person[i]==0){
+             return i;
+           }
+          
+          }
+      }
+        int kill = (k-1) % personleft;
+          while(kill>0){
+             
+              index = (index+1)%n;
+             if(person[index]==0){
+                  kill--;
+                 
+             } 
+          }
+      
+
+        person[index]=1;  //mark 1 which we eliminate
+          while(person[index]==1){
+                  index= (index+1)%n;
+              }
+          return winner(n,k,person ,personleft-1,index);
+    
     }
     public int findTheWinner(int n, int k) {
-       return winner(n,k)+1;
+        int person[] = new int[n];
+        for(int i=0;i<n;i++){
+           person[i]=0;
+        }
+        return winner(n,k,person,n,0)+1;
         
     }
 }
+// class Solution {
+//     public static int winner(int n ,int k ){
+//         if(n==1){
+//             return 0;
+//         }
+//         return (winner(n-1,k) + k)%n;
+//     }
+//     public int findTheWinner(int n, int k) {
+//        return winner(n,k)+1;
+        
+//     }
+// }
