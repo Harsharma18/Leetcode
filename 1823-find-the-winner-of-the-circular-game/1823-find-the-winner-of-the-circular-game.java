521e2552b4fdc508dda3a05997c16900cfd6d5fd
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-08-26 08:16:43 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (87.93%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int winner(int n ,int k ){
+        if(n==1){
+            return 0;
+        }
+        return (winner(n-1,k) + k)%n;
+    }
+    public int findTheWinner(int n, int k) {
+       return winner(n,k)+1;
+        
+    }
+}
