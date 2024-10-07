64f2a009d41b63248d2c25bb1693ec8b80527ad7
Harsharma18 <158789055+Harsharma18@users.noreply.github.com>
2024-10-07 16:22:37 +0530

    Time: 4 ms (95.80%), Space: 42.5 MB (92.57%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,35 +1,60 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder result = new StringBuilder();  // To store the reversed words
-        int i = s.length() - 1;  // Start from the end of the string
-
-        // Step 1: Process the string from the end
-        while (i >= 0) {
-            // Skip any trailing spaces
-            while (i >= 0 && s.charAt(i) == ' ') {
+        StringBuilder  sb = new StringBuilder();
+        int i = s.length()-1;
+        while(i>=0){
+            while(i>=0 && s.charAt(i) ==' '){
                 i--;
             }
-            
-            // If i is less than 0, we have processed the whole string
-            if (i < 0) {
-                break;
-            }
-            
-            // Step 2: Find the end of the current word
-            int wordEnd = i;
-            
-            // Move backward to find where the word starts
-            while (i >= 0 && s.charAt(i) != ' ') {
+            int wordStart = i;
+            while(i>=0 && s.charAt(i)!=' '){
                 i--;
             }
-            
-            // Step 3: Add the word to the result
-            if (result.length() > 0) {
-                result.append(" ");  // Add space between words
+            if(wordStart>=0){
+                if(sb.length()>0){
+                    sb.append(" ");
+                }
+               
+                    sb.append(s.substring(i+1,wordStart+1));
+                
             }
-            result.append(s.substring(i + 1, wordEnd + 1));  // Add the word (from i+1 to wordEnd)
         }
-        
-        return result.toString();  // Return the final reversed sentence
+        return sb.toString();
     }
 }
+//class Solution {
+//     public String reverseWords(String s) {
+//         int i = s.length() - 1;
+//         String result = "";
+
+//         // Skip  spaces
+//         while (i >= 0 && s.charAt(i) == ' ') {
+//             i--;
+//         }
+        
+//         int wordEnd = i;
+        
+//         while (i >= 0) {
+//             // Skip leading spaces for the current word
+//             while (i >= 0 && s.charAt(i) == ' ') {
+//                 i--;
+//             }
+//             int wordStart = i;
+
+//             // Find the start of the current word
+//             while (i >= 0 && s.charAt(i) != ' ') {
+//                 i--;
+//             }
+
+//             // Add the current word to the result
+//             if (wordStart >= 0) {
+//                 if (!result.isEmpty()) {
+//                     result += " "; // Add a space before the next word
+//                 }
+//                 result += s.substring(i + 1, wordStart + 1);
+//             }
+//         }
+        
+//         return result;
+//     }
+// }
